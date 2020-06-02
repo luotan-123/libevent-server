91c5868cfae0eeaebfe08325ac11ea31994a7efb
luotan-123 <935451190@qq.com>
2020-06-02 16:23:05 +0800

    优化项目架构

---
+++ linuxserverplatform/LinuxRun/libs/install_libs.sh
@@ -0,0 +1,18 @@
+cp ./libcrypto.so.10 /usr/local/lib/
+cp ./libcurl.so.4 /usr/local/lib/
+cp ./libevent-2.2.so.1 /usr/local/lib/
+cp ./libevent_pthreads-2.2.so.1 /usr/local/lib/
+cp ./libhiredis.a /usr/local/lib/
+cp ./libjson.so /usr/local/lib/
+cp ./libmysqlclient.so.20 /usr/local/lib/
+cp ./libssh2.so.1 /usr/local/lib/
+cp ./libssl.so.10 /usr/local/lib/
+
+echo "copy success !!!"
+
+ln -s /usr/local/lib/libcurl.so.4 /usr/local/lib/libcurl.so
+ln -s /usr/local/lib/libevent-2.2.so.1 /usr/local/lib/libevent.so
+ln -s /usr/local/lib/libevent_pthreads-2.2.so.1 /usr/local/lib/libevent_pthreads.so
+ln -s /usr/local/lib/libmysqlclient.so.20 /usr/local/lib/libmysqlclient.so
+
+echo "link success !!!"
